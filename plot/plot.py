da1152b59752aab8936c130e9953a1a40e6d9ccf
augusto <augustomafra1@gmail.com>
2019-10-01 22:55:13 -0300

    Enable plotting dates on cxviz plot

--- plot/plot.py
+++ plot/plot.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 import locale
 import matplotlib.pyplot
 import numpy
@@ -9,6 +10,7 @@ def set_locale():
 
 class Cxdb(object):
     def __init__(self, cxdb_path, fund):
+        self.fund = fund
         with open(os.path.join(cxdb_path, '{}'.format(fund)), 'r') as csvfile:
             csvreader = csv.reader(csvfile, delimiter=';', strict=True)
             self.parse_csv(csvreader)
@@ -25,19 +27,40 @@ class Cxdb(object):
                     self.data[col].append(line[i])
                     i += 1
 
+    def subplot(self, index, metric):
+        if index > 3:
+            raise Exception('Invalid subplot index')
+        matplotlib.pyplot.subplot(3, 1, index)
+        if index == 1:
+            matplotlib.pyplot.title(self.fund)
+        date_header = list(self.data.keys())[0]
+        matplotlib.pyplot.plot(date(self.data[date_header]),
+                               numeric(self.data[metric]))
+        matplotlib.pyplot.ylabel(metric)
+        matplotlib.pyplot.xlabel(date_header)
+        matplotlib.pyplot.grid(True)
+
 def numeric(data_array):
     return [numpy.nan if i == '-' else locale.atof(i) for i in data_array]
 
-def subplot(index, fund, metric, data):
-    if index > 3:
-        raise Exception('Invalid subplot index')
-    matplotlib.pyplot.subplot(3, 1, index)
-    if index == 1:
-        matplotlib.pyplot.title(fund)
-    matplotlib.pyplot.plot(data)
-    matplotlib.pyplot.ylabel(metric)
-    matplotlib.pyplot.xlabel('Data')
-    matplotlib.pyplot.grid(True)
+def date(data_array):
+    d = {'weekday' : '%a',
+            'month' : '%b',
+            'day' : '%d',
+            'year' : '%Y',
+            'hour' : '%H',
+            'minute' : '%M',
+            'second' : '%S',
+            'zone' : '%Z',
+            'offset' : '%z',
+            'ignore' : '%f'} # HACK Actually %f stands for microseconds
+    hour_format = ':'.join([d['hour'], d['minute'], d['second']])
+    zone_format = d['zone'] + d['offset']
+    ignore = '(-{})'.format(d['ignore'])
+    date_format = ' '.join([d['weekday'], d['month'], d['day'], d['year'],
+                            hour_format, zone_format, ignore])
+
+    return [datetime.strptime(i, date_format) for i in data_array]
 
 def show_feed(cxdb_path, fund):
     cxdb = Cxdb(cxdb_path, fund)
@@ -51,8 +74,8 @@ def show_feed(cxdb_path, fund):
               'Acumulado 12M (%)',
               'PL (milhões R$)',
               'PL Médio (milhões R$)']
-    subplot(1, fund, header[4], numeric(cxdb.data[header[4]]))
-    subplot(2, fund, header[5], numeric(cxdb.data[header[5]]))
-    subplot(3, fund, header[6], numeric(cxdb.data[header[6]]))
+    cxdb.subplot(1, header[4])
+    cxdb.subplot(2, header[5])
+    cxdb.subplot(3, header[6])
     matplotlib.pyplot.show()
 
