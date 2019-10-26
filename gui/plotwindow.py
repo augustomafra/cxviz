0fd0f7f09f0a2620543d80197dbc82ff3f34290f
augusto <augustomafra1@gmail.com>
2019-10-26 13:46:13 -0300

    Add close button to plot windows

--- gui/plotwindow.py
+++ gui/plotwindow.py
@@ -10,13 +10,19 @@ class PlotWindow(tk.Frame):
         super().__init__(parent)
 
         self.pack(side=tk.LEFT, expand=False)
+
+        tk.Button(self,
+                  text='X',
+                  bg='white',
+                  command=self.destroy).pack(side=tk.TOP, anchor=tk.E)
+
         figure = matplotlib.figure.Figure(figsize=(5, 5), dpi=100)
         plot = figure.add_subplot(1, 1, 1)
         plot.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
 
         canvas = FigureCanvasTkAgg(figure, self)
         canvas.show()
-        canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         toolbar = NavigationToolbar2TkAgg(canvas, self)
         toolbar.update()
